dfdb8abf8d60c3180135bbff2b0f18b91537c7ef
Rui Xue <rui.xue@fasolutions.com>
2025-02-05 15:44:28 +0200

    #5 copy function separated

--- python/rowsCopy.py
+++ python/rowsCopy.py
@@ -5,7 +5,7 @@ def readYaml(filename):
   with open(filename, 'r') as file:
     data = yaml.safe_load(file)
   return data
-def createConnection(dbConfig, name, autoCommit=True):
+def _createConnection(dbConfig, name, autoCommit=True):
    conn = dbConfig[name]
    return mariadb.connect(
         user=conn["username"], 
@@ -15,6 +15,17 @@ def createConnection(dbConfig, name, autoCommit=True):
         database=conn["schema"],
         autocommit=autoCommit,
         ssl="")
+def copy(configFileName:str, sourceDatabaseAlias:str, targetDatabaseAlias:str, query:str):
+  dbConfig = readYaml(configFileName)
+  sourceConn = _createConnection(dbConfig, sourceDatabaseAlias)
+  selectedSourceCursor = sourceConn.cursor()
+  selectedSourceCursor.execute(query)
+  targetConn = _createConnection(dbConfig, targetDatabaseAlias)
+  insertCursor = targetConn.cursor()
+  inputData = [list(r) for r in selectedSourceCursor.fetchall()]
+  print(inputData)
+  #insertCursor.executemany("INSERT INTO xxx VALUES (?,?,?,?,?)", inputData)
+
 def main():
   try:
     parser = argparse.ArgumentParser(
@@ -22,18 +33,14 @@ def main():
                     description='Copy row(s) from one database to another',
                     epilog='Text at the bottom of help')
     parser.add_argument('-cf', '--configfile', help="Give configuration file name")
+    parser.add_argument('-q', '--query', help="Query to retrieve rows from source database")
+
     args = parser.parse_args()
     configFileName = "connectionsConfig.yml"
     if args.configfile:
       configFileName = args.configfile
-    dbConfig = readYaml(configFileName)
-    sourceConn = createConnection(dbConfig, "dev")
-    selectedSourceCursor = sourceConn.cursor()
-    selectedSourceCursor.execute("select * from qrtz_cron_triggers where trigger_name = 'SCHEDULED_PROCESS-processLauncher-91623755459453'")
-    targetConn = createConnection(dbConfig, "local")
-    insertCursor = targetConn.cursor()
-    inputData = [list(r) for r in selectedSourceCursor.fetchall()]
-    insertCursor.executemany("INSERT INTO qrtz_cron_triggers VALUES (?,?,?,?,?)", inputData)
+
+    copy(configFileName, 'dev','local', args.query)
   except mariadb.Error as e:
     print(f"ERROR!!!!!!!!!!: {e}")
 
